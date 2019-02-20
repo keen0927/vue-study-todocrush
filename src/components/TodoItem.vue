580ab74d71d973d3a28881172bbc57c03c264702
KEEN <kyh0927@live.co.kr>
2019-02-21 00:37:33 +0900

    2019 02 20 #2

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="todo-item" :class="{'is-complete':todo.completed}">
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         <p>
-            <Checkbox @change="markComplete">{{todo.title}}</Checkbox>
-
-
+            <Checkbox @on-change="markComplete">{{todo.title}}</Checkbox>
+            <!--<input type="checkbox" @change="markComplete" />{{todo.title}}-->
+            <Button @click="$emit('del-todo',todo.id)" type="error" ghost>삭제</Button>
         </p>
     </div>
 </template>
@@ -26,8 +26,7 @@
     padding: 10px;
     border-bottom: 1px solid #ccc;
 }
-.is-complete {
+.is-complete label{
     text-decoration: line-through;
-
 }
 </style>
